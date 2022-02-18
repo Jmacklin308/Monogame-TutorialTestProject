94b7d4bafd98e2f34969c48ccb90c2a3b3061b69
justin macklin <jmacklin308@gmail.com>
2022-02-18 17:16:32 -0600

    added basic input

--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,10 @@ namespace TutorialTestProject
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         
-        //sprites
+        //player
         private Texture2D playerSpriteSheet;
+        private Vector2 playerPos;
+        private float playerSpeed;
 
         public Game1()
         {
@@ -23,6 +25,9 @@ namespace TutorialTestProject
         {
             // TODO: Add your initialization logic here
 
+            playerPos = new Vector2(_graphics.PreferredBackBufferWidth * 0.5f, _graphics.PreferredBackBufferHeight * 0.5f);
+            playerSpeed = 10.0f;
+
             base.Initialize();
         }
 
@@ -32,27 +37,46 @@ namespace TutorialTestProject
 
             // TODO: use this.Content to load your game content here
             playerSpriteSheet = Content.Load<Texture2D>("character-animations");
-
         }
 
         protected override void Update(GameTime gameTime)
         {
-            //close the game if escape or back on a controler
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
 
-            // TODO: Add your update logic here
+            CheckPlayerInput((float)gameTime.ElapsedGameTime.TotalSeconds);
 
             base.Update(gameTime);
         }
 
+        private void CheckPlayerInput(float totalSeconds)
+        {
+              
+            var keyboardState = Keyboard.GetState();
+            var gamepadState = GamePad.GetState(PlayerIndex.One);
+            
+            //keyboard input
+            if (keyboardState.IsKeyDown(Keys.W))
+                playerPos.Y -= playerSpeed * totalSeconds;
+            if (keyboardState.IsKeyDown(Keys.S))
+                playerPos.Y += playerSpeed * totalSeconds;
+            if (keyboardState.IsKeyDown(Keys.A))
+                playerPos.X -= playerSpeed * totalSeconds;
+            if (keyboardState.IsKeyDown(Keys.D))
+                playerPos.X += playerSpeed * totalSeconds;
+            
+            
+            //close the game 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+        }
+
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.WhiteSmoke);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(playerSpriteSheet, new Vector2(0,0),Color.White);
+            _spriteBatch.Draw(playerSpriteSheet, playerPos,Color.White);
             _spriteBatch.End();
             
             base.Draw(gameTime);
